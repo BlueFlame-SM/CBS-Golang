a957bbcd924d9f28efbe9c54dbb4e454334c0ce1
Timo Post <timo.post@student.uva.nl>
2023-05-23 12:53:27 +0200

    Added GO-runfct generator and variable-type funcon
    + generate.sh generates GO-runfct
    + variable-type funcon for retrieving type of a variable

--- GO-runfct/app/Funcons/Golang/GO2Types/GO2Types.hs
+++ GO-runfct/app/Funcons/Golang/GO2Types/GO2Types.hs
@@ -12,52 +12,70 @@ types = typeEnvFromList
     []
 
 funcons = libFromList
-    [("int",NullaryFuncon stepInt),("int8",NullaryFuncon stepInt8),("int16",NullaryFuncon stepInt16),("int32",NullaryFuncon stepInt32),("uint",NullaryFuncon stepUint),("uint8",NullaryFuncon stepUint8),("uint16",NullaryFuncon stepUint16),("uint32",NullaryFuncon stepUint32)]
-
-int_ = FName "int"
-stepInt = evalRules [rewrite1] []
-    where rewrite1 = do
-            let env = emptyEnv
-            rewriteTermTo (TName "int32") env
+    [("int8",NullaryFuncon stepInt8),("int16",NullaryFuncon stepInt16),("int32",NullaryFuncon stepInt32),("int",NullaryFuncon stepInt32),("int64",NullaryFuncon stepInt64),("uint8",NullaryFuncon stepUint8),("uint16",NullaryFuncon stepUint16),("uint32",NullaryFuncon stepUint32),("uint",NullaryFuncon stepUint32),("uint64",NullaryFuncon stepUint64),("variable-type",StrictFuncon stepVariable_type),("is-subtype",StrictFuncon stepIs_subtype)]
 
 int8_ = FName "int8"
 stepInt8 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TSortPower (TFuncon (FValue (Nat (-2)))) (TFuncon (FValue (Nat 7))),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 7))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TApp "int-neg" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 7))]],TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 7))],TFuncon (FValue (Nat (-1)))]]) env
 
 int16_ = FName "int16"
 stepInt16 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TSortPower (TFuncon (FValue (Nat (-2)))) (TFuncon (FValue (Nat 15))),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 15))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TApp "int-neg" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 15))]],TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 15))],TFuncon (FValue (Nat (-1)))]]) env
 
 int32_ = FName "int32"
+int_ = FName "int32"
 stepInt32 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TSortPower (TFuncon (FValue (Nat (-2)))) (TFuncon (FValue (Nat 31))),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 31))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TApp "int-neg" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 31))]],TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 31))],TFuncon (FValue (Nat (-1)))]]) env
 
-uint_ = FName "uint"
-stepUint = evalRules [rewrite1] []
+int64_ = FName "int64"
+stepInt64 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TName "uint32") env
+            rewriteTermTo (TApp "bounded-ints" [TApp "int-neg" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 63))]],TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 63))],TFuncon (FValue (Nat (-1)))]]) env
 
 uint8_ = FName "uint8"
 stepUint8 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 8))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 8))],TFuncon (FValue (Nat (-1)))]]) env
 
 uint16_ = FName "uint16"
 stepUint16 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 16))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 16))],TFuncon (FValue (Nat (-1)))]]) env
 
 uint32_ = FName "uint32"
+uint_ = FName "uint32"
 stepUint32 = evalRules [rewrite1] []
     where rewrite1 = do
             let env = emptyEnv
-            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-sub" [TSortPower (TFuncon (FValue (Nat 2))) (TFuncon (FValue (Nat 32))),TFuncon (FValue (Nat 1))]]) env
+            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 32))],TFuncon (FValue (Nat (-1)))]]) env
+
+uint64_ = FName "uint64"
+stepUint64 = evalRules [rewrite1] []
+    where rewrite1 = do
+            let env = emptyEnv
+            rewriteTermTo (TApp "bounded-ints" [TFuncon (FValue (Nat 0)),TApp "int-add" [TApp "int-pow" [TFuncon (FValue (Nat 2)),TFuncon (FValue (Nat 64))],TFuncon (FValue (Nat (-1)))]]) env
+
+variable_type_ fargs = FApp "variable-type" (fargs)
+stepVariable_type fargs =
+    evalRules [rewrite1] []
+    where rewrite1 = do
+            let env = emptyEnv
+            env <- vsMatch fargs [PADT "variable" [VPAnnotated VPWildCard (TName "locations"),VPAnnotated (VPMetaVar "T") (TName "types")]] env
+            rewriteTermTo (TVar "T") env
+
+is_subtype_ fargs = FApp "is-subtype" (fargs)
+stepIs_subtype fargs =
+    evalRules [rewrite1] []
+    where rewrite1 = do
+            let env = emptyEnv
+            env <- vsMatch fargs [VPAnnotated (VPMetaVar "T1") (TName "types"),VPAnnotated (VPMetaVar "T2") (TName "types")] env
+            rewriteTermTo (TName "true") env
